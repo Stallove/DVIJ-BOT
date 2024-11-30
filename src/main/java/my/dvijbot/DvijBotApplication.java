151a9743cc918998fe231286c965c003cd92a57e
SKondrashov <s.kondrashov@andersenlab.com>
2024-11-30 12:56:38 +0200

    Initial commit

---
+++ src/main/java/my/dvijbot/DvijBotApplication.java
@@ -0,0 +1,13 @@
+package my.dvijbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvijBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvijBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/dvijbot/DvijBotApplicationTests.java
@@ -0,0 +1,13 @@
+package my.dvijbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvijBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
